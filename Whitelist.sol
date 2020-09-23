7ede66032ab6748257f1870728f653198a4467c5
Helenekng <39047359+Helenekng@users.noreply.github.com>
2020-09-23 18:12:36 +0200

    Update Whitelist.sol

--- Whitelist.sol
+++ Whitelist.sol
@@ -39,10 +39,10 @@ contract Whitelist is Ownable {
 
 
   /**
-   * @dev add an address to the whitelist
+   * @dev add an address to the chosen whitelist
    * @param addr address
    * @param whitelistName list to which we want to add an address
-   * @return success true if the address was added to the whitelist, false if the address was already in the whitelist
+   * @return success true if the address was added to the chosen whitelist, false if the address was already in the whitelist
    */
   function addAddressToWhitelist(address addr, string memory whitelistName) onlyOwner public returns(bool success) {
     if(keccak256(abi.encodePacked((whitelistName))) == keccak256(abi.encodePacked(("technicians")))){
@@ -63,10 +63,10 @@ contract Whitelist is Ownable {
 
 
   /**
-   * @dev add addresses to the whitelist
+   * @dev add addresses to the chosen whitelist
    * @param addrs addresses
    * @param whitelistName list to which we want to add addresses
-   * @return success true if at least one address was added to the whitelist,
+   * @return success true if at least one address was added to the chosen whitelist,
    * false if all addresses were already in the whitelist
    */
   function addAddressesToWhitelist(address[] memory addrs, string memory whitelistName) onlyOwner public returns(bool success) {
@@ -79,10 +79,10 @@ contract Whitelist is Ownable {
 
 
   /**
-   * @dev remove an address from the whitelist
+   * @dev remove an address from the chosen whitelist
    * @param addr address
    * @param whitelistName list from which we want to remove an address
-   * @return success true if the address was removed from the whitelist,
+   * @return success true if the address was removed from the chosen whitelist,
    * false if the address wasn't in the whitelist in the first place
    */
   function removeAddressFromWhitelist(address addr, string memory whitelistName) onlyOwner public returns(bool success) {
@@ -106,10 +106,10 @@ contract Whitelist is Ownable {
 
 
   /**
-   * @dev remove addresses from the whitelist
+   * @dev remove addresses from the chosen whitelist
    * @param addrs addresses
    * @param whitelistName list from which we want to remove addresses
-   * @return success true if at least one address was removed from the whitelist,
+   * @return success true if at least one address was removed from the chosen whitelist,
    * false if all addresses weren't in the whitelist in the first place
    */
   function removeAddressesFromWhitelist(address[] memory addrs, string memory whitelistName) onlyOwner public returns(bool success) {
